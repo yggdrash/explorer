4d449dd2c07863e453405f66c357e570acb80155
Luke Hwang <luke@r2v.io>
2018-08-29 16:03:11 +0900

    Edit block page

--- src/views/BlockPage.vue
+++ src/views/BlockPage.vue
@@ -1,15 +1,50 @@
 <template>
   <div>
-    blocks
-    {{ $route.params.id }}
-    <pre>
+    <template v-if="$route.params.id == undefined">
+      <v-data-table
+              :headers="headers"
+              :items="blocks"
+              :pagination.sync="pagination"
+      >
+        <template slot="items" slot-scope="props">
+          <td><router-link :to="'blocks/' + props.item.index">
+            {{ props.item.index }}</router-link></td>
+          <td><router-link :to="'blocks/' + props.item.hash">
+            {{ props.item.hash }}</router-link></td>
+          <td>{{ props.item.author }}</td>
+          <td>{{ props.item.dataSize }}</td>
+          <td>{{ props.item.timestamp }}</td>
+          <td>{{ props.item.body.length }}</td>
+        </template>
+      </v-data-table>
+    </template>
+    <template v-else>
+      <pre>
       {{ blockDetail }}
-    </pre>
+      </pre>
+    </template>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
 export default {
+  data () {
+    return {
+      headers: [
+        { text: 'Block #', sortable: false},
+        { text: 'Block Hash', sortable: false},
+        { text: 'Author', sortable: false},
+        { text: 'Size', sortable: false},
+        { text: 'Date', sortable: false},
+        { text: 'Block # of TXs', sortable: false}
+      ],
+
+      pagination: {
+        rowsPerPage: 25
+      }
+    }
+  },
+
   computed: {
     ...mapState([
       'blocks'
@@ -19,10 +54,11 @@ export default {
       console.log(this.$route.params.id)
       if(this.$route.params.id) {
         return this.blocks.filter(b => {
-          if (Number.isInteger(this.$route.params.id)) {
-            return b.index == this.$route.params.id
+          console.log(isNaN(this.$route.params.id));
+          if (isNaN(this.$route.params.id)) {
+            return b.hash === this.$route.params.id
           }
-          return b.hash == this.$route.params.id
+          return String(b.index) === this.$route.params.id
         })
       }
       return this.blocks
