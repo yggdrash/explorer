5ede05549e0cab221dc36b70e702a0dc810bc7f3
Luke Hwang <luke@r2v.io>
2018-09-05 18:51:22 +0900

    Add BranchList
    - Add BranchCard

---
+++ src/components/BranchCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card :color="color" class="white--text">
+    <v-card-title primary-title>
+      <div class="headline">{{ branchInfo.name }}</div>
+      <div style="word-break: break-word">{{ branchInfo.id }}</div>
+    </v-card-title>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn flat dark>
+        EXPLORE
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+  export default {
+    props: [
+      'color', 'branchInfo'
+    ]
+  }
+</script>
---
+++ src/views/BranchList.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid
+               grid-list-md>
+    <div class="font-weight-black display-2 mb-2">YGGDRASH Branches</div>
+    <v-layout row wrap>
+      <v-flex xs4 v-for="(b, index) in branches" :key="b.id">
+        <BranchCard :branch-info="b" :color="colors[index % colors.length]" />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  import { mapState } from 'vuex'
+  import BranchCard from '../components/BranchCard'
+
+  export default {
+    components: {
+      BranchCard,
+    },
+
+    data: () => ({
+      colors: [ 'blue-grey darken-2', 'secondary', 'cyan darken-2', 'purple' ]
+    }),
+
+    computed: {
+      ...mapState([
+        'branches'
+      ])
+    }
+  }
+</script>
