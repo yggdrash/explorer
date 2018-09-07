cdbd665aed433bfc4832fc93a8577cada0918f83
Luke Hwang <luke@r2v.io>
2018-09-07 18:07:41 +0900

    Add chip component

--- src/views/Branch.vue
+++ src/views/Branch.vue
@@ -7,6 +7,26 @@
           <span class="font-italic grey--text">
             <strong>{{ currentBranch.id.slice(0, 16) }}</strong>{{ currentBranch.id.slice(16) }}
           </span>
+          <div>
+            <v-chip color="green" text-color="white" small v-if="currentBranch.name === 'STEM'">
+              <v-avatar>
+                <v-icon>cloud_done</v-icon>
+              </v-avatar>
+              Active
+            </v-chip>
+            <v-chip color="orange" text-color="white" small v-else>
+              <v-avatar>
+                <v-icon>wb_incandescent</v-icon>
+              </v-avatar>
+              Concept
+            </v-chip>
+            <v-chip color="indigo" text-color="white" small v-if="currentBranch.name === 'YEED'">
+              <v-avatar>
+                <v-icon>flash_on</v-icon>
+              </v-avatar>
+              Developing
+            </v-chip>
+          </div>
         </v-flex>
         <router-view></router-view>
       </v-layout>
