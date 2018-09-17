c1638fc852cefdb8eddfd525d7c1e65b2c401197
Luke Hwang <luke@r2v.io>
2018-09-17 16:01:29 +0900

    Edit title margin

--- src/views/BranchList.vue
+++ src/views/BranchList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <div class="font-weight-black display-2 mb-2">YGGDRASH Branches</div>
+    <div class="font-weight-black display-2 mb-4">YGGDRASH Branches</div>
     <BranchCardList :branches="branchesExcludeStem"/>
   </v-container>
 </template>
