e5fa99ca9199edf80c4a1a95fa0fd881a7eb29a4
Luke Hwang <luke@r2v.io>
2018-09-18 19:47:01 +0900

    Edit 404 page

--- src/views/NotFoundComponent.vue
+++ src/views/NotFoundComponent.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-layout>
-    <v-flex>
-      page not found.
-    </v-flex>
-  </v-layout>
+  <v-container fill-height grid-list-xs class="text-xs-center">
+    <img src="@/assets/images/404.png" style="margin: 0 auto" class="mt-4">
+  </v-container>
 </template>
